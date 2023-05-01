9382ba7641e1a92b69f12ca2db19a4780f906310
yosra-wan <74923855+yosra-wan@users.noreply.github.com>
2023-05-01 09:45:44 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://adminyosra:postgresql@database-1.ch8pfajcfdcn.us-east-1.rds.amazonaws.com/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres000@database-1.ch8pfajcfdcn.us-east-1.rds.amazonaws.com/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
